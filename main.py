c85f03aa8c036069241a72791de26251f16bde21
jojojaey <122209392+jojojaey@users.noreply.github.com>
2026-06-17 20:40:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 # ضع التوكين الخاص بك هنا
 TOKEN ='8802340199:AAFAs9C-V2qYlIZIxnBWX-SGN4r46JnY740'
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ツೄྀالسلام عليك اهلا بك في بوت اشتراك جيمني!⊹يرجى الانتضار ليقوم احد الادارين بالتواصل معك وشكراً للتواصل معنا واختيارنا")
-    ("")
+    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        f"السلام عليك {update.effective_user.first_name}، أهلاً بك في بوت اشتراك جيمني.\n"
+        "يرجى الانتظار ليقوم أحد الإداريين بالتواصل معك.\n"
+        "شكراً لتواصلك معنا واختيارنا!")
+        
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
