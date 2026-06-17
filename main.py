128db4977b03608cedd12d9da4b484a29ed2d717
jojojaey <122209392+jojojaey@users.noreply.github.com>
2026-06-17 22:21:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 # ضع التوكين الخاص بك هنا
 TOKEN ='8802340199:AAFAs9C-V2qYlIZIxnBWX-SGN4r46JnY740'
 
-await update.message.reply_text:(
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
         f"السلام عليك {update.effective_user.first_name}، أهلاً بك في بوت اشتراك جيمني.\n"
         "يرجى الانتظار ليقوم أحد الإداريين بالتواصل معك.\n"
         "شكراً لتواصلك معنا واختيارنا!")
