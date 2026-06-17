ed77c7ed862ef46100a9c3968587611f60db02df
jojojaey <122209392+jojojaey@users.noreply.github.com>
2026-06-17 21:03:18 +0300

    Fix formatting of welcome message in main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ TOKEN ='8802340199:AAFAs9C-V2qYlIZIxnBWX-SGN4r46JnY740'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text(
-     f"السلام عليك {update.effective_user.first_name}، أهلاً بك في بوت اشتراك جيمني.\n"
-    "يرجى الانتظار ليقوم أحد الإداريين بالتواصل معك.\n"
-    "شكراً لتواصلك معنا واختيارنا!")
+      f"السلام عليك {update.effective_user.first_name}، أهلاً بك في بوت اشتراك جيمني.\n"
+      "يرجى الانتظار ليقوم أحد الإداريين بالتواصل معك.\n"
+      "شكراً لتواصلك معنا واختيارنا!")
         
 
 if __name__ == '__main__':
