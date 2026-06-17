e690cc924ccda69e69e84efb39a18abec73a6412
jojojaey <122209392+jojojaey@users.noreply.github.com>
2026-06-17 19:52:44 +0300

    Update welcome message with new text and emojis

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN ='8802340199:AAFAs9C-V2qYlIZIxnBWX-SGN4r46JnY740'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("السلام عليك اهلا بك في بوت اشتراك جيمني!يرجى الانتضار ليقوم احد الادارين بالتواصل معك وشكراً للتواصل معنا واختيارنا")
+    await update.message.reply_text("ツೄྀالسلام عليك اهلا بك في بوت اشتراك جيمني!⊹يرجى الانتضار ليقوم احد الادارين بالتواصل معك وشكراً للتواصل معنا واختيارنا")
     ("")
 
 if __name__ == '__main__':
