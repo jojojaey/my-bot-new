8989cde7d251cfd17a886a57838c626db0d4c500
jojojaey <122209392+jojojaey@users.noreply.github.com>
2026-06-18 10:06:02 +0300

    Change Telegram bot token
    Updated the bot token for the Telegram bot.

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 users_list = set()
-TOKEN = '8802340199:AAFAs9C-V2qY1IZIxnBWX-SGN4r46JnY740'
-
+TOKEN ='8802340199:AAHCSMzIc8NB04QR8UHbQjarnOC3ia7hdAk'
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     users_list.add(f"{user.first_name} (ID: {user.id})")
     await update.message.reply_text(f"السلام عليك {user.first_name}، أهلاً بك في بوت اشتراك جيمني. يرجى الانتظار ليقوم أحد الإداريين بالتواصل معك. شكراً لتواصلك معنا واختيارنا!")
-
 async def show_users(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not users_list:
         await update.message.reply_text("لا يوجد أعضاء حالياً.")
     else:
         message = "قائمة الأعضاء:\n" + "\n".join(users_list)
         await update.message.reply_text(message)
-
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler('start', start))
